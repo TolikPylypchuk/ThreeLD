44dc42b58a5b5f7ba284cdd4ab954592c10df51f
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-04-19 22:36:52 +0300

    Added tests for EditEvents

--- ThreeLD.Tests/Editor/EditEventTests.cs
+++ ThreeLD.Tests/Editor/EditEventTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Web.Mvc;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -16,6 +14,7 @@ namespace ThreeLD.Tests.Editor
 {
 	[TestClass]
 	[SuppressMessage("ReSharper", "ImplicitlyCapturedClosure")]
+	[SuppressMessage("ReSharper", "UnusedVariable")]
 	public class EditEventTests
 	{
 		[TestMethod]
@@ -32,21 +31,143 @@ namespace ThreeLD.Tests.Editor
 				Description = "A test event",
 				Category = "Test"
 			};
-
-			var events = new[] { eventToUpdate }.AsQueryable();
-
+			
 			var mock = new Mock<IRepository<Event>>();
 			mock.Setup(repo => repo.GetById(eventToUpdate.Id))
 				.Returns(eventToUpdate);
-			mock.Setup(repo => repo.Update(eventToUpdate))
-				.Callback(() => events = new[] { eventToUpdate }.AsQueryable());
-			mock.Setup(repo => repo.Save()).Returns(1);
 
 			var controller = new EditorController(mock.Object);
 
 			var result = controller.EditEvent(eventToUpdate.Id);
 
 			Assert.AreSame(eventToUpdate, (Event)result.Model);
+
+			mock.Verify(repo => repo.GetById(eventToUpdate.Id), Times.Once());
+		}
+
+		[TestMethod]
+		public void EditEventPostValidTest()
+		{
+			var eventToUpdate = new Event
+			{
+				Id = 1,
+				Name = "Test",
+				Address = "Test",
+				DateTime = DateTime.Now,
+				Duration = TimeSpan.FromHours(2),
+				Url = "https://www.event1.test.com",
+				Description = "An updated test event",
+				Category = "Test"
+			};
+
+			var events = new[]
+			{
+				new Event
+				{
+					Id = 1,
+					Name = "Test",
+					Address = "Test",
+					DateTime = DateTime.Now,
+					Duration = TimeSpan.FromHours(2),
+					Url = "https://www.event1.test.com",
+					Description = "A test event",
+					Category = "Test"
+				}
+			};
+
+			var mock = new Mock<IRepository<Event>>();
+			mock.Setup(repo => repo.Update(eventToUpdate))
+				.Callback(() => events[0] = eventToUpdate);
+			mock.Setup(repo => repo.Save()).Returns(1);
+
+			var controller = new EditorController(mock.Object);
+
+			controller.Validate(eventToUpdate);
+
+			var result = controller.EditEvent(eventToUpdate);
+
+			Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+
+			Assert.AreSame(eventToUpdate, events[0]);
+
+			mock.Verify(repo => repo.Update(eventToUpdate), Times.Once());
+			mock.Verify(repo => repo.Save(), Times.Once());
+		}
+
+		[TestMethod]
+		public void EditEventPostMessageTest()
+		{
+			var eventToUpdate = new Event
+			{
+				Name = "Test",
+				Address = "Test",
+				DateTime = DateTime.Now,
+				Duration = TimeSpan.FromHours(2),
+				Url = "https://www.event1.test.com",
+				Description = "A test event",
+				Category = "Test"
+			};
+
+			var mock = new Mock<IRepository<Event>>();
+			mock.Setup(repo => repo.Update(eventToUpdate));
+			mock.Setup(repo => repo.Save()).Returns(1);
+
+			var controller = new EditorController(mock.Object);
+
+			controller.Validate(eventToUpdate);
+
+			var result = controller.EditEvent(eventToUpdate);
+
+			Assert.IsNotNull(controller.TempData["message"]);
+		}
+
+		[TestMethod]
+		public void EditEventPostInvalidTest()
+		{
+			var eventToUpdate = new Event
+			{
+				Id = 1,
+				Name = "Test",
+				Address = "Updated Test",
+				DateTime = DateTime.Now,
+				Duration = TimeSpan.FromHours(2),
+				Url = "https://www.event1.test.com"
+			};
+
+			var originalEvent = new Event
+			{
+				Id = 1,
+				Name = "Test",
+				Address = "Test",
+				DateTime = DateTime.Now,
+				Duration = TimeSpan.FromHours(2),
+				Url = "https://www.event1.test.com",
+				Description = "A test event",
+				Category = "Test"
+			};
+
+			var events = new[] { originalEvent };
+
+			var mock = new Mock<IRepository<Event>>();
+			mock.Setup(repo => repo.Update(eventToUpdate))
+				.Callback(() => events[0] = eventToUpdate);
+			mock.Setup(repo => repo.Save()).Returns(1);
+
+			var controller = new EditorController(mock.Object);
+
+			controller.Validate(eventToUpdate);
+
+			var result = controller.EditEvent(eventToUpdate);
+
+			Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+			var viewResult = (ViewResult)result;
+
+			Assert.AreSame(originalEvent, events[0]);
+			Assert.AreSame(eventToUpdate, viewResult.Model);
+
+			mock.Verify(repo => repo.Update(eventToUpdate), Times.Never());
+			mock.Verify(repo => repo.Save(), Times.Never());
 		}
 	}
 }
