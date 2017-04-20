1b07ee5c74742b006bb81d48dfe388afca8dbad6
Artur Zayats <Artur Zayats>
2017-04-20 03:02:58 +0300

    ONLY functionality to SignIn, SignOut, SignUp(from Admin menu)

--- ThreeLD.Web/Controllers/HomeController.cs
+++ ThreeLD.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 using ThreeLD.DB.Models;
 using ThreeLD.DB.Repositories;
@@ -13,10 +14,32 @@ namespace ThreeLD.Web.Controllers
 		{
 			this.events = events;
 		}
-
-		public ActionResult Index()
+        
+        public ActionResult Index()
 		{
 			return this.View(this.events.GetAll());
 		}
-	}
+
+        [Authorize]
+        public ActionResult IndexLogin()
+        {
+            return View(GetData("IndexLogin"));
+        }
+        [Authorize(Roles = "Users")]
+        public ActionResult OtherAction()
+        {
+            return View("IndexLogin", GetData("OtherAction"));
+        }
+        private Dictionary<string, object> GetData(string actionName)
+        {
+            Dictionary<string, object> dict
+            = new Dictionary<string, object>();
+            dict.Add("Action", actionName);
+            dict.Add("User", HttpContext.User.Identity.Name);
+            dict.Add("Authenticated", HttpContext.User.Identity.IsAuthenticated);
+            dict.Add("Auth Type", HttpContext.User.Identity.AuthenticationType);
+            dict.Add("In Users Role", HttpContext.User.IsInRole("Users"));
+            return dict;
+        }
+    }
 }
