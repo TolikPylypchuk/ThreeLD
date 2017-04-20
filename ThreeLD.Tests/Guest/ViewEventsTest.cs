2a4f45f9e6416db8c899d802cb43113988e306b5
JuilaZvizlo <zvjuli24@gmail.com>
2017-04-21 00:13:00 +0300

    created GuestController + ViewEvents() and FilterEvents() methods

---
+++ ThreeLD.Tests/Guest/ViewEventsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using ThreeLD.DB.Models;
+using ThreeLD.DB.Repositories;
+using ThreeLD.Web.Controllers;
+
+namespace ThreeLD.Tests.Guest
+{
+    [TestClass]
+    public class ViewEventsTest
+    {
+        [TestMethod]
+        public void ViewEventsGetTest()
+        {
+        }
+    }
+}
---
+++ ThreeLD.Web/Controllers/GuestController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+using ThreeLD.DB.Models;
+using ThreeLD.DB.Repositories;
+
+namespace ThreeLD.Web.Controllers
+{
+    [Authorize(Roles = "Guest")]
+    public class GuestController : Controller
+    {
+        private IRepository<Event> events;
+
+        public GuestController(IRepository<Event> events)
+        {
+            this.events = events;
+        }
+
+        [HttpGet]
+        public ViewResult ViewEvents()
+        {
+            return this.View(this.events.GetAll().ToList<Event>());
+        }
+
+        [HttpGet]
+        public ViewResult FilterEventsByCategory(string[] category)
+        {
+            List<Event> result = new List<Event>();
+
+            for (int i = 0; i < category.Length; i++)
+            {
+                var currentEvents = this.events.GetAll().Where(e => e.Category == category[i]).Select(e => e).ToList<Event>();
+
+                if (currentEvents != null)
+                {
+                    for (int j = 0; j < currentEvents.Count; j++)
+                    {
+                        result.Add(currentEvents[i]);
+                    }
+                }
+
+                currentEvents = null;
+            }
+            
+            return this.View(result);
+        }
+
+        [HttpGet]
+        public ViewResult FilterEventsByDateTime(DateTime dateFrom,
+            DateTime dateTo)
+        {
+            List<Event> result = new List<Event>();
+            var currentEvents = this.events.GetAll().
+                Where(e => (DateTime.Compare(e.DateTime, dateFrom) >= 0 && 
+                DateTime.Compare(e.DateTime, dateTo) <= 0)).Select(e => e);
+
+            if (currentEvents != null)
+            {
+                result = currentEvents.ToList<Event>();
+            }
+
+            return this.View(result);
+        }
+    }
+}
