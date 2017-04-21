dd184a8855cfeb897632a56aff4fbe1da95cfde5
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-04-21 08:37:03 +0300

    Minor fixes

--- ThreeLD.Web/Models/ViewModels/UserViewModels.cs
+++ ThreeLD.Web/Models/ViewModels/UserViewModels.cs
@@ -4,37 +4,40 @@ using ThreeLD.DB.Models;
 
 namespace ThreeLD.Web.Models.ViewModels
 {
-    public class CreateModel
-    {
-        [Required]
-        public string UserName { get; set; }
-        [Required]
-        public string FirstName { get; set; }
-        [Required]
-        public string LastName { get; set; }
-        [Required]
-        public string Email { get; set; }
-        [Required]
-        public string Password { get; set; }
-    }
-    public class LoginModel
-    {
-        [Required]
-        public string UserName { get; set; }
-        [Required]
-        public string Password { get; set; }
-    }
-    public class RoleEditModel
-    {
-        public AppRole Role { get; set; }
-        public IEnumerable<User> Members { get; set; }
-        public IEnumerable<User> NonMembers { get; set; }
-    }
-    public class RoleModificationModel
-    {
-        [Required]
-        public string RoleName { get; set; }
-        public string[] IdsToAdd { get; set; }
-        public string[] IdsToDelete { get; set; }
-    }
+	public class CreateModel
+	{
+		[Required]
+		public string UserName { get; set; }
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Password { get; set; }
+	}
+
+	public class LoginModel
+	{
+		[Required]
+		public string UserName { get; set; }
+		[Required]
+		public string Password { get; set; }
+	}
+
+	public class RoleEditModel
+	{
+		public AppRole Role { get; set; }
+		public IEnumerable<User> Members { get; set; }
+		public IEnumerable<User> NonMembers { get; set; }
+	}
+
+	public class RoleModificationModel
+	{
+		[Required]
+		public string RoleName { get; set; }
+		public string[] IdsToAdd { get; set; }
+		public string[] IdsToDelete { get; set; }
+	}
 }
