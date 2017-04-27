7f4de49c3502aeca6552fe971831159372f9bb95
JuilaZvizlo <zvjuli24@gmail.com>
2017-04-27 18:45:33 +0300

    local commit

---
+++ ThreeLD.Tests/Guest/ViewEventsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ThreeLD.DB.Models;
+using ThreeLD.Web.Controllers;
+
+namespace ThreeLD.Tests.Guest
+{
+    [TestClass]
+    public class ViewEventsTests
+    {
+        [TestMethod]
+        public void ViewEventsGetTest()
+        {
+            GuestController controller = new GuestController
+        }
+    }
+}
