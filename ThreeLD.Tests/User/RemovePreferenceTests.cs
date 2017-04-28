c0b12e3a50143c1e4c5664ddf2ab9c665305df24
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-04-28 03:34:28 +0300

    Fixed the RemoveExistentPreferenceTest

--- ThreeLD.Tests/User/RemovePreferenceTests.cs
+++ ThreeLD.Tests/User/RemovePreferenceTests.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Web.Mvc;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
-using System.Collections.Generic;
-using System.Web.Mvc;
 
 using ThreeLD.DB.Models;
 using ThreeLD.DB.Repositories;
@@ -10,103 +12,99 @@ using ThreeLD.Web.Controllers;
 
 namespace ThreeLD.Tests.User
 {
-    [TestClass]
-    public class RemovePreferenceTests
-    {
-        [TestMethod]
-        public void RemoveExistentPreferenceTest()
-        {
-            const int preferenceToRemoveId = 1;
+	[TestClass]
+	[SuppressMessage("ReSharper", "UnusedVariable")]
+	public class RemovePreferenceTests
+	{
+		[TestMethod]
+		public void RemoveExistentPreferenceTest()
+		{
+			const int preferenceToRemoveId = 1;
 
-            List<Preference> preferences = new List<Preference>
-            {
-                new Preference()
-                {
-                    Id = 1,
-                    Category = "test",
-                    UserId = "testUserId"
-                }
-            };
+			var preferences = new List<Preference>
+			{
+				new Preference
+				{
+					Id = 1,
+					Category = "test",
+					UserId = "testUserId"
+				}
+			};
 
-            Mock<IRepository<Preference>> mockRepository = 
-                new Mock<IRepository<Preference>>();
+			var mockRepository = new Mock<IRepository<Preference>>();
+			
+			mockRepository.Setup(r => r.Delete(preferenceToRemoveId))
+				.Callback(() => 
+					preferences.RemoveAll(p => p.Id == preferenceToRemoveId));
+			mockRepository.Setup(r => r.Save()).Returns(1);
 
-            mockRepository.Setup(r => r.GetById(preferenceToRemoveId))
-                .Returns(preferences[0]);
-            mockRepository.Setup(r => r.Delete(preferenceToRemoveId))
-                .Callback(() => 
-                    preferences.RemoveAll(p => p.Id == preferenceToRemoveId));
-            mockRepository.Setup(r => r.Save()).Returns(1);
+			var controllerContext = new Mock<ControllerContext>();
 
-            var controllerContext = new Mock<ControllerContext>();
+			var controller =
+				new UserController(mockRepository.Object, null)
+				{
+					ControllerContext = controllerContext.Object
+				};
 
-            var controller =
-                new UserController(mockRepository.Object, null)
-                {
-                    ControllerContext = controllerContext.Object
-                };
+			Assert.AreEqual(1, preferences.Count);
 
-            Assert.AreEqual(1, preferences.Count);
+			var viewResult = controller.RemovePreference(preferenceToRemoveId);
 
-            var viewResult = controller.RemovePreference(preferenceToRemoveId);
+			Assert.AreEqual(0, preferences.Count);
+			Assert.IsNotNull(controller.TempData["message"]);
+			Assert.IsNull(controller.TempData["error"]);
+			
+			mockRepository.Verify(
+				r => r.Delete(preferenceToRemoveId), Times.Once());
+			mockRepository.Verify(r => r.Save(), Times.Once());
+		}
 
-            Assert.AreEqual(0, preferences.Count);
-            Assert.IsNotNull(controller.TempData["message"]);
-            Assert.IsNull(controller.TempData["error"]);
+		[TestMethod]
+		public void RemoveNonExistentPreferenceTest()
+		{
+			const int preferenceToRemoveId = 2;
 
-            mockRepository.Verify(
-                r => r.GetById(preferenceToRemoveId), Times.Once());
-            mockRepository.Verify(
-                r => r.Delete(preferenceToRemoveId), Times.Once());
-            mockRepository.Verify(r => r.Save(), Times.Once());
-        }
+			var preferences = new List<Preference>
+			{
+				new Preference
+				{
+					Id = 1,
+					Category = "test",
+					UserId = "testUserId"
+				}
+			};
 
-        [TestMethod]
-        public void RemoveNonExistentPreferenceTest()
-        {
-            const int preferenceToRemoveId = 2;
+			var mockRepository =
+				new Mock<IRepository<Preference>>();
 
-            List<Preference> preferences = new List<Preference>
-            {
-                new Preference()
-                {
-                    Id = 1,
-                    Category = "test",
-                    UserId = "testUserId"
-                }
-            };
+			mockRepository.Setup(r => r.GetById(preferenceToRemoveId))
+				.Returns((Preference)null);
+			mockRepository.Setup(r => r.Delete(preferenceToRemoveId))
+				.Callback(() =>
+					preferences.RemoveAll(p => p.Id == preferenceToRemoveId));
+			mockRepository.Setup(r => r.Save()).Returns(0);
 
-            Mock<IRepository<Preference>> mockRepository =
-                new Mock<IRepository<Preference>>();
+			var controllerContext = new Mock<ControllerContext>();
 
-            mockRepository.Setup(r => r.GetById(preferenceToRemoveId))
-                .Returns((Preference)null);
-            mockRepository.Setup(r => r.Delete(preferenceToRemoveId))
-                .Callback(() =>
-                    preferences.RemoveAll(p => p.Id == preferenceToRemoveId));
-            mockRepository.Setup(r => r.Save()).Returns(0);
+			var controller =
+				new UserController(mockRepository.Object, null)
+				{
+					ControllerContext = controllerContext.Object
+				};
 
-            var controllerContext = new Mock<ControllerContext>();
+			Assert.AreEqual(1, preferences.Count);
 
-            var controller =
-                new UserController(mockRepository.Object, null)
-                {
-                    ControllerContext = controllerContext.Object
-                };
+			var viewResult = controller.RemovePreference(preferenceToRemoveId);
 
-            Assert.AreEqual(1, preferences.Count);
+			Assert.AreEqual(1, preferences.Count);
+			Assert.IsNull(controller.TempData["message"]);
+			Assert.IsNotNull(controller.TempData["error"]);
 
-            var viewResult = controller.RemovePreference(preferenceToRemoveId);
-
-            Assert.AreEqual(1, preferences.Count);
-            Assert.IsNull(controller.TempData["message"]);
-            Assert.IsNotNull(controller.TempData["error"]);
-
-            mockRepository.Verify(
-                r => r.GetById(preferenceToRemoveId), Times.Never());
-            mockRepository.Verify(
-                r => r.Delete(preferenceToRemoveId), Times.Once());
-            mockRepository.Verify(r => r.Save(), Times.Once());
-        }
-    }
+			mockRepository.Verify(
+				r => r.GetById(preferenceToRemoveId), Times.Never());
+			mockRepository.Verify(
+				r => r.Delete(preferenceToRemoveId), Times.Once());
+			mockRepository.Verify(r => r.Save(), Times.Once());
+		}
+	}
 }
