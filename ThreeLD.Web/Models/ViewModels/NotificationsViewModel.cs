d07078d0c2dc5523ecee4bb172efcab32526d25a
Bozhena Telepko <t.bozhena1234@gmail.com>
2017-05-03 14:51:51 +0300

    Added NotificationsViewModel and View for Notifications.

---
+++ ThreeLD.Web/Models/ViewModels/NotificationsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+using ThreeLD.DB.Models;
+
+
+namespace ThreeLD.Web.Models.ViewModels
+{
+    [ExcludeFromCodeCoverage]
+    public class NotificationsViewModel
+    {
+        public List<Notification> UnreadNotifications { get; set; }
+        public List<Notification> ReadNotifications { get; set; }
+    }
+}
